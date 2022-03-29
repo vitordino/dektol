fafdac45c609d859b48b117e5cbee04cc0afdb81
Vitor Dino <me@vitordino.com>
2022-03-29 04:15:54 -0300

    :zap: memoize blurhash

--- src/utils/blurhash.ts
+++ src/utils/blurhash.ts
@@ -1,11 +1,14 @@
 import sharp from 'sharp'
 import { encode, decode } from 'blurhash'
 import { createCanvas, loadImage, Image } from 'canvas'
+import memoize from 'p-memoize'
 
 type Dimensions = { width: number; height: number }
 type CanvasOptions = { size: number; quality: number; channels: 1 | 2 | 4 }
 
 const DEFAULT_OPTIONS = { size: 16, quality: 40, channels: 4 } as const
+
+const toBase64 = (s: string) => Buffer.from(s).toString('base64')
 
 const getImageData = (image: Image) => {
   const canvas = createCanvas(image.width, image.height)
@@ -16,8 +19,8 @@ const getImageData = (image: Image) => {
 
 const generateBlurhashStringFromUrl = async (url: string, channels = DEFAULT_OPTIONS.channels) => {
   const image = await loadImage(url)
-  console.log(' image = ', image.width, image.height)
   const imageData = getImageData(image)
+  console.log('[generateBlurhashStringFromUrl]:', toBase64(url))
   return encode(imageData.data, imageData.width, imageData.height, channels, channels)
 }
 
@@ -34,14 +37,21 @@ const generateImageURIFromHash = async (
     .jpeg({ overshootDeringing: true, quality: options.quality })
     .toBuffer() // Here also possible to do whatever with your image, e.g. save it or something else.
 
+  console.log('[generateImageURIFromHash]:', hash)
   return `data:image/jpeg;base64,${resizedImageBuf.toString('base64')}`
 }
 
-export const getBlurhashURIFromUrl = async (
+type GetBlurhashURIFromUrl = (
   url?: string | null,
   dimensions?: Dimensions | null,
-) => {
+) => Promise<string | undefined>
+
+export const getBlurhashURIFromUrl: GetBlurhashURIFromUrl = async (url, dimensions) => {
   if (!url || !dimensions) return
   const blurhashString = await generateBlurhashStringFromUrl(url)
   return generateImageURIFromHash(blurhashString, dimensions)
 }
+
+export const memoizedBlurhash = memoize(getBlurhashURIFromUrl, {
+  cacheKey: k => /(?=[^\/]+$).+(?=\?)/g.exec(k[0] || '')?.[0] || k[0],
+})
