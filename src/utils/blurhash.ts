de96a6d7b42dcc26df09f747a29b25cbffceeeb5
Vitor Dino <me@vitordino.com>
2022-03-29 02:42:02 -0300

    :zap: faster blurhash

--- src/utils/blurhash.ts
+++ src/utils/blurhash.ts
@@ -3,9 +3,9 @@ import { encode, decode } from 'blurhash'
 import { createCanvas, loadImage, Image } from 'canvas'
 
 type Dimensions = { width: number; height: number }
-type CanvasOptions = { size: number; quality: number }
+type CanvasOptions = { size: number; quality: number; channels: 1 | 2 | 4 }
 
-const DEFAULT_OPTIONS = { size: 16, quality: 40 }
+const DEFAULT_OPTIONS = { size: 16, quality: 40, channels: 4 } as const
 
 const getImageData = (image: Image) => {
   const canvas = createCanvas(image.width, image.height)
@@ -14,11 +14,11 @@ const getImageData = (image: Image) => {
   return context.getImageData(0, 0, image.width, image.height)
 }
 
-const generateBlurhashStringFromUrl = async (url: string) => {
+const generateBlurhashStringFromUrl = async (url: string, channels = DEFAULT_OPTIONS.channels) => {
   const image = await loadImage(url)
   console.log(' image = ', image.width, image.height)
   const imageData = getImageData(image)
-  return encode(imageData.data, imageData.width, imageData.height, 4, 4)
+  return encode(imageData.data, imageData.width, imageData.height, channels, channels)
 }
 
 const generateImageURIFromHash = async (
@@ -29,7 +29,7 @@ const generateImageURIFromHash = async (
   const hashWidth = options.size
   const hashHeight = Math.round(hashWidth * (height / width))
   const pixels = decode(hash, hashWidth, hashHeight)
-  const raw = { channels: 4, width: hashWidth, height: hashHeight } as const
+  const raw = { channels: options.channels, width: hashWidth, height: hashHeight } as const
   const resizedImageBuf = await sharp(Buffer.from(pixels), { raw })
     .jpeg({ overshootDeringing: true, quality: options.quality })
     .toBuffer() // Here also possible to do whatever with your image, e.g. save it or something else.
