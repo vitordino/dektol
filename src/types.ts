c1d1318372acfa368614a502ef27550f28484381
Vitor Dino <me@vitordino.com>
2022-04-17 13:36:36 -0300

    :recycle: rewrite page query to use graphql

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+import { ColorField, ImageField } from '@prismicio/types'
+export { ColorField, ImageField } from '@prismicio/types'
+
+export type Edges<T> = { edges: { node: T }[] }
+
+export type Colors = {
+  background: ColorField
+  foreground: ColorField
+}
+
+export type Meta = { _meta: { uid: string } }
+
+export type HorizontalSlice = {
+  type: 'horizontal'
+  primary: Colors
+  fields: { image: ImageField }[]
+}
+
+export type UnknownSlice = { type: string }
+
+export type Slice = HorizontalSlice | UnknownSlice
+export type Page = Meta & Colors & { body: Slice[] }
+export type HomePage = { allPages: Edges<Page> }
---
+++ src/utils/getHomePageData.ts
@@ -0,0 +1,47 @@
+import * as prismic from '@prismicio/client'
+import { gql } from 'graphql-request'
+import type { HomePage } from '~/types'
+import { createClient } from '~/prismic'
+
+const client = createClient()
+
+const homePageQuery = gql`
+  {
+    allPages {
+      edges {
+        node {
+          _meta {
+            uid
+          }
+          background
+          foreground
+          body {
+            __typename
+            ... on PageBodyHorizontal {
+              type
+              primary {
+                background
+                foreground
+              }
+              fields {
+                image
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const endpoint = prismic.getGraphQLEndpoint('photo-vitordino')
+const url = `${endpoint}?query=${homePageQuery}`
+
+const getHomePageData = async () => {
+  const response = await client.graphQLFetch(url, { method: 'GET' })
+  const body = (await response.json()) as { data: HomePage }
+  console.log(body)
+  return body.data.allPages.edges.map(({ node }) => node)
+}
+
+export default getHomePageData
