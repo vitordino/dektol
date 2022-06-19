fb59042600de32efa3e87a1f3567a66c847b2ad8
Vitor Dino <vitor@kunstdesign.com.br>
2022-06-19 02:11:54 -0300

    :sparkles: add prismic-ts-codegen on get-custom-types script

--- scripts/get-custom-types.ts
+++ scripts/get-custom-types.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import fetch from 'node-fetch'
+import { execa } from 'execa'
 import { writeFile } from 'fs/promises'
 import { join } from 'path'
 import { oraPromise as loader } from 'ora'
@@ -14,9 +15,6 @@ type CustomType = {
   status: boolean
 }
 
-const s = (object: unknown) => JSON.stringify(object, null, 2)
-const jsonPath = (fileName: string) => join('custom_types', `${fileName}.json`)
-
 if (!PRISMIC_REPO) {
   throw new Error('no PRISMIC_REPO on .env')
 }
@@ -24,17 +22,21 @@ if (!PRISMIC_TYPES_TOKEN) {
   throw new Error('no PRISMIC_TYPES_TOKEN on .env')
 }
 
-const headers = {
-  repository: PRISMIC_REPO,
-  Authorization: `Bearer ${PRISMIC_TYPES_TOKEN}`,
-}
+const s = (object: unknown) => JSON.stringify(object, null, 2)
+const jsonPath = (fileName: string) => join('custom_types', `${fileName}.json`)
 
-export default (async () => {
-  const x = await loader(fetch('https://customtypes.prismic.io/customtypes', { headers }), {
+const fetchAllCustomTypes = async () => {
+  const headers = {
+    repository: PRISMIC_REPO,
+    Authorization: `Bearer ${PRISMIC_TYPES_TOKEN}`,
+  }
+  const response = await loader(fetch('https://customtypes.prismic.io/customtypes', { headers }), {
     text: 'pulling custom types',
   })
-  const allCustomTypes = (await loader(x.json(), { text: 'decoding' })) as CustomType[]
+  return (await loader(response.json(), { text: 'decoding' })) as CustomType[]
+}
 
+const writeCustomTypeIndexFile = async (allCustomTypes: CustomType[]) => {
   const indexFileContent = allCustomTypes
     .filter(({ status }) => status)
     .map(({ id, label, repeatable }) => ({
@@ -45,13 +47,29 @@ export default (async () => {
       value: `${id}.json`,
     }))
 
-  await loader(writeFile(jsonPath('index'), s(indexFileContent)), {
+  return await loader(writeFile(jsonPath('index'), s(indexFileContent)), {
     text: 'writing index file',
   })
+}
 
+const writeCustomTypeFiles = async (allCustomTypes: CustomType[]) =>
   allCustomTypes.forEach(async ({ id, json }) => {
-    console.log(`writing ${id}.json`)
     await loader(writeFile(jsonPath(id), s(json)), { text: `writing ${id}.json` })
-    console.log(`wrote ${id}.json`)
   })
+
+const codegen = async () => {
+  const { exitCode, stdout, stderr } = await loader(execa('prismic-ts-codegen'), {
+    text: 'generating typescript definitions',
+  })
+
+  if (stdout) process.stdout.write(stdout)
+  if (stderr) process.stderr.write(stderr)
+  if (exitCode) process.exit(exitCode)
+}
+
+export default (async () => {
+  const allCustomTypes = await fetchAllCustomTypes()
+  await writeCustomTypeIndexFile(allCustomTypes)
+  await writeCustomTypeFiles(allCustomTypes)
+  await codegen()
 })()
