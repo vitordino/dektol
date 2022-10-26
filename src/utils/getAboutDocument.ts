9c9efa4021f90b9b0c27878ea9a2c44645dc921c
vitordino <me@vitordino.com>
2022-10-26 02:46:10 -0300

    :sparkles: start about page

---
+++ src/utils/getAboutDocument.ts
@@ -0,0 +1,15 @@
+import memoize from 'p-memoize'
+
+import type { AboutDocument } from '../prismic.generated'
+import { createClient } from '../prismic'
+
+const client = createClient()
+
+type GetAboutDocument = () => Promise<(AboutDocument['data'] & { uid: string }) | null>
+
+const getAboutDocument: GetAboutDocument = async () => {
+  const { results } = await client.getByType<AboutDocument>('about')
+  return { ...results?.[0]?.data, uid: results[0].uid } ?? null
+}
+
+export default memoize(getAboutDocument)
